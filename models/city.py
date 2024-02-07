cfc417bbd0d4c9a70268037a3ae6f1f857010329
ADIOZ DANIEL <adiozdaniel@gmail.com>
2024-02-07 06:06:39 +0300

    city.py

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    City Module
+"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """City class for AirBnB"""
+    state_id = ""
+    name = ""
