67a73b6126dfc0179c605a450d2c27ead7b4a886
ADIOZ DANIEL <adiozdaniel@gmail.com>
2024-02-08 07:08:24 +0300

    updated Base_model

--- models/base_model.py
+++ models/base_model.py
@@ -2,72 +2,91 @@
 """
     The Base_Module!
 """
-
 from uuid import uuid4
 from datetime import datetime
-import json
+import models
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from os import environ
 
-class BaseModel:
-    """BaseModel class for AirBnB"""
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
+if (storage_engine == "db"):
+    Base = declarative_base()
+else:
+    Base = object
+
+
+class BaseModel(Base):
+    """
+        The Base class that defines all common attributes and methods
+        for subsequent classes
+    """
+
+    __tablename__ = 'base_model'
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
-        """Initialization method"""
+        """
+            To initialize the BaseModel
+        """
         if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != '__class__':
-                    setattr(self, key, value)
+            for key in kwargs:
+                if key == "__class__":
+                    continue
+                elif key in ("created_at", "updated_at"):
+                    iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+                    try:
+                      setattr(self, key, datetime.strptime(kwargs[key], iso_format))
+                    except ValueError:
+                      """
+                          Handle invalid date instead of pass
+                      """
+                      print("Invalid datetime format for {}: {}".format(key, kwargs[key]))
+                else:
+                    setattr(self, key, kwargs[key])
+                self.id = str(uuid4())
+            if "id" not in kwargs:
+                self.id = str(uuid4())
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """String representation method"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """
+            Then return string representation of the Model
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """Save method to update updated_at"""
+        """
+            to update time of the model
+        """
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """Dictionary representation method"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """
+            the custom representation of a model
+        """
+        custom_dict = {
+            "__class__": self.__class__.__name__,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
+        for key, value in self.__dict__.items():
+            if key not in ("id", "_sa_instance_state"):
+                custom_dict[key] = value
+        return custom_dict
 
-class FileStorage:
-    """FileStorage class for serializing and deserializing instances to/from JSON"""
-    __file_path = "file.json"
-    __objects = {}
-
-    def all(self):
-        """Returns the dictionary __objects"""
-        return FileStorage.__objects
-
-    def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
-
-    def save(self):
-        """Serializes __objects to the JSON file"""
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump({key: obj.to_dict() for key, obj in FileStorage.__objects.items()}, file)
-
-    def reload(self):
-        """Deserializes the JSON file to __objects"""
-        try:
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    class_name, obj_id = key.split('.')
-                    obj_dict = {k: v if k in ['created_at', 'updated_at'] else v for k, v in value.items()}
-                    obj_dict['created_at'] = datetime.strptime(obj_dict['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                    obj_dict['updated_at'] = datetime.strptime(obj_dict['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                    cls = globals()[class_name]
-                    self.new(cls(**obj_dict))
-        except FileNotFoundError:
-            pass
+    def delete(self):
+        """
+            To delete the current instance from storage
+        """
+        k = "{}.{}".format(type(self).__name__, self.id)
+        del models.storage.__objects[k]
