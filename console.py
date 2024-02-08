7d35e87537b7a829d8a43fcceb1c940b471cf5bf
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-02-08 12:31:13 +0100

    Added the quit and EOF implementation to the console

--- console.py
+++ console.py
@@ -48,6 +48,15 @@ class HBNBCommand(cmd.Cmd):
         "Review"
     }
 
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
